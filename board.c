b6ea5b3c4082902b3fd56fc68e30ca4447f619b1
TRAZEOG <thomaslarosemasson@gmail.com>
2025-01-29 23:18:42 +0100

    changed the board's implem

--- board.c
+++ board.c
@@ -11,14 +11,23 @@
 board* init_board(int row, int col) {
     clear();
     board* b = malloc(sizeof(board));
-    b->board = malloc(sizeof(wchar_t*) * row);
+    b->board = malloc(sizeof(Bloc) * row);
     for (int i = 0; i < row; i++) {
-        b->board[i] = malloc(sizeof(wchar_t) * col);
+        b->board[i] = malloc(sizeof(Bloc) * col);
         for (int j = 0; j < col; j++) {
             if (i == 0 || i == row-1 || j == 0 || j == col-1) {
-                b->board[i][j] = L'\U0001F9F1';
-            } else {
-                b->board[i][j] = L' ';
+                if ((i == 0 || i == row-1) && (j == col/2 || j == col/2 - 1) || (j == 0 || j == col-1) && (i == row/2 || i == row/2 - 1)) {
+                    b->board[i][j].type = GATE;
+                    b->board[i][j].sprite = SPRITE_GATE;
+                }
+                else {
+                    b->board[i][j].type = WALL;
+                    b->board[i][j].sprite = SPRITE_WALL;
+                }
+            }
+            else {
+                b->board[i][j].type = VOID;
+                b->board[i][j].sprite = SPRITE_VOID;
             }
         }
     }
--- board.h
+++ board.h
@@ -5,10 +5,35 @@
 #include <stdlib.h>
 #include "unitech.h"
 
+
+typedef enum {
+    VOID = 0,
+    WALL = 1,
+    ENEMY = 2,
+    LOOT = 3,
+    GATE = 4,
+    NB_TYPES = 5,
+} Type;
+
+
+typedef enum {
+    SPRITE_VOID,
+    SPRITE_PLAYER,
+    SPRITE_WALL,
+    SPRITE_ENEMY,
+    SPRITE_LOOT,
+    SPRITE_GATE
+} Sprite;
+
+typedef struct {
+    Type type;
+    Sprite sprite;
+} Bloc;
+
 typedef struct {
     int row;
     int col;
-    wchar_t** board;
+    Bloc** board;
 } board;
 
 board* init_board(int row, int col);
