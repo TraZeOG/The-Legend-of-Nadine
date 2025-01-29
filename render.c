a06d7de507ee3958aeceead021dab13844cc8fd6
TRAZEOG <thomaslarosemasson@gmail.com>
2025-01-29 23:20:38 +0100

    more efficient render

--- render.c
+++ render.c
@@ -2,22 +2,31 @@
 
 
 
+const wchar_t SPRITES[] = {
+    L' ',         // SPRITE_VOID
+    L'\U0001F9D9', // SPRITE_PLAYER 🧙
+    L'\U0001F9F1', // SPRITE_WALL 🧱
+    L'\U0001F9DF', // SPRITE_ENEMY 🧟
+    L'\U0001F48E', // SPRITE_LOOT 💎
+    L'\U0001F6AA'  // SPRITE_GATE 🚪
+};
+
 /**
  * @brief Prints the board to the console.
  * @param b A pointer to the board structure to be printed.
  */
 void print_board(board* b) {
-    clear();
     setlocale(LC_ALL, "");
+    wprintf(L"\033[H");
     for (int i = 0; i < b->row; i++) {
         for (int j = 0; j < b->col; j++) {
-            if (b->board[i][j] == L' ') {
+            if (b->board[i][j].sprite == SPRITE_VOID) {
                 wprintf(L"  ");
             } else {
-                wprintf(L"%lc", b->board[i][j]);
+                wprintf(L"%lc", SPRITES[b->board[i][j].sprite]);
             }
         }
         wprintf(L"\n");
-        fflush(stdout);
     }
+    fflush(stdout); 
 }
--- render.h
+++ render.h
@@ -3,6 +3,8 @@
 
 #include "player.h"
 
+extern const wchar_t SPRITES[];
+
 void print_board(board* b);
 void print_player(Coord* player, board board);
 
