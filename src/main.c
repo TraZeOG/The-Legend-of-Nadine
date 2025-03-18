1b32e68352f6842ecc816cdc4b42d2b0e25e83d8
Thomas_STL <thomaslarosemasson@gmail.com>
2025-03-18 12:09:59 +0100

    removed music, matches requirements

--- src/main.c
+++ src/main.c
@@ -9,9 +9,9 @@ int main() {
     system("osascript -e 'tell application \"Terminal\" to activate' -e 'tell application \"System Events\" to keystroke \"f\" using {command down, control down}'");
     wprintf(L"\033[?25l");
     clear();
-    SDL_Init(SDL_INIT_AUDIO);
-    Mix_Music* music = init_music("assets/music/music.mp3");
-    play_music(music);
+    //SDL_Init(SDL_INIT_AUDIO);
+    //Mix_Music* music = init_music("assets/music/music.mp3");
+    //play_music(music);
     int run = true;
     while (run) {
         if (in_menus) {
